e3609ebacc6965dc796a8092bb1f9a70b6d41559
Josué <psjosue1@gmail.com>
2023-03-09 22:44:13 -0600

    plantilla de asesoria tecnica

---
+++ addPages/Asesoria-tecnica.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Metadatos SEO -->
+    <title>Los Tecnologicos</title>
+    <meta name="description" content="Descripción de la página">
+    <meta name="keywords" content="palabras clave, separadas, por, comas">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot" content="index,follow">
+
+    <!-- Cache -->
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+
+    <!-- Metadatos para redes sociales -->
+    <meta property="og:title" content="Título de la página">
+    <meta property="og:description" content="Descripción de la página">
+    <meta property="og:image" content="URL de la imagen destacada">
+    <meta property="og:url" content="URL de la página">
+    <meta property="og:type" content="website">
+
+    <!-- Metadatos para redes sociales TWITER -->
+    <meta property="twitter:site_name" content="" />
+    <meta property="twitter:title" content="" />
+    <meta property="twitter:description" content="" />
+    <meta property="twitter:image" content="" />
+
+    <!-- Otros metadatos -->
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <!-- ESTILOS EN CSS -->
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" media="(min-width:320px)" href="../css/Asesoria-tecnica/movil.css" />
+    <link rel="stylesheet" media="(min-width:768px)" href="../css/Asesoria-tecnica/tablet.css" />
+    <link rel="stylesheet" media="(min-width:1024px)" href="../css/Asesoria-tecnica/desktop.css" />
+
+    <!-- CONEXION CON BOOTSTRAP v5.3 -->
+    <link rel="stylesheet" href="../bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css">
+
+    <!-- SLIDER GENERAL -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css'>
+</head>
+
+
+<body>
+    <!-----------------HEADER----------------------->
+    <?php require('../componentesPHP/headerDos.php')?>
+
+ 
+
+
+    
+    <!-----------------fOOTER----------------------->
+    <?php require('../componentesPHP/footerDos.php')?>
+
+
+    <!-- Formulario -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../js/formulario.js"></script>
+
+    <!-- Slider camaras -->
+    <script src="../js/carrusel-Individual.js"></script>
+
+    <!-- div expand-->
+    <script src="../js/divExpand.js"></script>
+
+    <!-- principal js -->
+    <script src="https://kit.fontawesome.com/6c240dc0e0.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c56cdedc82.js" crossorigin="anonymous"></script>
+    <script src="../js/main.js"></script>
+    <script src="../bootstrap-5.3.0-alpha1-dist/js/bootstrap.min.js"></script>
+
+    <!-- Slider general -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js'></script>
+    <script src="../js/cards.js"></script>
+
+</body>
+
+</html>
